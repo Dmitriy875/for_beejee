5285f0d48407cd2f6965abb6699a7d6b3df0559d
Dmitriy875 <solleks777@gmail.com>
2020-04-22 23:08:40 +0800

    @design Some design anв content on index-page.

--- view/templates/zen/content.php
+++ view/templates/zen/content.php
@@ -1,5 +1,28 @@
 <body>
-йа контент
+<div class="container">
+  <div class="jumbotron">
+    <h1 class="display-3">Zen</h1>
+    <!-- <p class="lead"></p> -->
+    <hr class="m-y-md">
+    <!-- <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> -->
+    <!-- <p class="lead"> -->
+      <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
+    </p>
 
+  </div>
+</div>
+<div class="row justify-content-start justify-content-center justify-content-end">
+  <div class="col-md-6">
+    <div class="alert alert-info" role="alert">
+      <a href="#" class="alert-link">Sort by: User</a> | <a href="#" class="alert-link">Sort by: Email</a> | <a href="#" class="alert-link">Sort by: Status</a>
+    </div>
+    <div class="alert alert-light" role="alert">
+      <strong>To user: name</strong> (< Email >) <a href="#" class="alert-link">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</a>.
+    </div>
+    <div class="alert alert-light" role="alert">
+      <strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>.
+    </div>
+  </div>
+</div>
 </body>
 </html>
